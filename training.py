d750e7dd19e4576ed0c900268b9248177a95faea
twu54 <twu54@illinois.edu>
2018-04-16 19:58:02 -0500

    Updated trainer for train test set

--- training.py
+++ training.py
@@ -4,15 +4,24 @@ from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.ensemble import RandomForestClassifier
 
-
-if __name__ == '__main__':
-
-    with open('training', 'rb') as fp:
+def load_dataset(f):
+    with open(f, 'rb') as fp:
         features, labels = map(np.array, pickle.load(fp))
     features = features[:, :8].astype(float)
-    print('Receiving feature of shape: {}, label of shape: {}'.format(features.shape, labels.shape))
+    print('Feature shape: {}, label shape: {}, #positive {}'.format(features.shape, labels.shape, sum(labels)))
+    return features, labels
 
-    classifier = RandomForestClassifier(n_estimators=20, n_jobs=-1)
+
+if __name__ == '__main__':
+    features, labels = load_dataset('training')
+    classifier = RandomForestClassifier(n_estimators=20, n_jobs=-1, class_weight="balanced")
     predicted = cross_val_predict(classifier, features, labels, cv=10, n_jobs=-1)
     stats = precision_recall_fscore_support(y_true=labels, y_pred=predicted, average='binary', pos_label=1)[:3]
+    print('precision: {}, recall: {}, f1-score: {}'.format(*stats))
+
+    # In case you would like a traditional training and testing
+    tfeatures, tlabels = load_dataset('testing')
+    classifier.fit(features, labels)
+    predicted = classifier.predict(tfeatures)
+    stats = precision_recall_fscore_support(y_true=tlabels, y_pred=predicted, average='binary', pos_label=1)[:3]
     print('precision: {}, recall: {}, f1-score: {}'.format(*stats))
